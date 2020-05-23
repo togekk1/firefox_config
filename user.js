0dbb592cb6c8df3126d28acb0398af6cb514750d
Simon.Cheng <simon.cheng@advantech.com.tw>
2020-05-23 10:06:39 +0800

    Update user.js

--- user.js
+++ user.js
@@ -9,20 +9,17 @@ user_pref("mousewheel.min_line_scroll_amount", 22);
 user_pref("toolkit.scrollbox.horizontalScrollDistance", 4);
 user_pref("toolkit.scrollbox.verticalScrollDistance", 5);
 
-// user_pref("gfx.webrender.all", true);
 user_pref("ui.systemUsesDarkTheme", 1);
 user_pref("privacy.donottrackheader.enabled", true);
 user_pref("privacy.resistFingerprinting", true);
 user_pref("devtools.theme", "dark");
 user_pref("reader.color_scheme", "dark");
-user_pref("reader.parse-on-load.force-enabled", true);
 user_pref("apz.frame_delay.enabled", false);
 user_pref("browser.cache.disk.enable", false);
 user_pref("browser.cache.disk_cache_ssl", false);
 user_pref("browser.cache.memory.capacity", 4194304);
 user_pref("browser.cache.memory.max_entry_size", 4194304);
 user_pref("layout.word_select.eat_space_to_next_word", false);
-// user_pref("network.trr.bootstrapAddress", "2a07:a8c0::7e:7b6c");
 user_pref("network.trr.uri", "https://9.9.9.9/dns-query");
 user_pref("network.trr.mode", 3);
 user_pref("mousewheel.min_line_scroll_amount", 45);
@@ -36,9 +33,6 @@ user_pref("browser.search.openintab", true);
 user_pref("browser.urlbar.openintab", true);
 user_pref("browser.tabs.insertAfterCurrent", true);
 user_pref("browser.tabs.insertRelatedAfterCurrent", false);
-user_pref("signon.generation.available", true);
-user_pref("signon.generation.enabled", true);
-// user_pref("javascript.options.shared_memory", true);
 user_pref("gfx.offscreencanvas.enabled", true);
 user_pref("signon.management.page.enabled", true);
 user_pref("accessibility.force_disabled", 1);
@@ -48,7 +42,6 @@ user_pref("browser.ping-centre.telemetry", false);
 user_pref("geo.provider.ms-windows-location", false);
 user_pref("network.dns.disablePrefetchFromHTTPS", false);
 user_pref("browser.send_pings.require_same_host", true);
-// user_pref("network.dns.disableIPv6", true);
 user_pref("network.http.altsvc.enabled", false);
 user_pref("network.http.altsvc.oe", false);
 user_pref("privacy.firstparty.isolate", true);
@@ -76,8 +69,6 @@ user_pref("browser.pagethumbnails.capturing_disabled", true);
 user_pref("security.ssl.disable_session_identifiers", true);
 
 user_pref("browser.sessionstore.restore_pinned_tabs_on_demand", true);
-// user_pref("gfx.webrender.split-render-roots", false);
-user_pref("browser.urlbar.megabar", true);
 user_pref("browser.display.background_color", "#202020");
 
 user_pref("browser.ssb.enabled", true);
@@ -91,7 +82,7 @@ user_pref("browser.display.show_image_placeholders", false);
 user_pref("security.dialog_enable_delay", 0);
 user_pref("reader.parse-on-load.enabled", false);
 
-user_pref("network.trr.excluded-domains", 'simon.es.wise-paas.cn,ableclub.advantech.com.tw,employeezone.advantech.com.tw,aclelearning.advantech.com.tw,portal-mp-ensaas.bm.wise-paas.com.cn,portal-catalog-ensaas.bm.wise-paas.com.cn,simon.bm.wise-paas.com.cn,portal-sso-ensaas.bm.wise-paas.com.cn,portal-mpsimon-ews001.bm.wise-paas.com.cn,portal-service-ensaas.bm.wise-paas.com.cn,portal-sso-ews001.bm.wise-paas.com.cn,portal-catalog-ews001.bm.wise-paas.com.cn,api-mp-ensaas.bm.wise-paas.com.cn,mailapp.advantech.com.tw,portal-mp-ews001.bm.wise-paas.com.cn,portal-order-ensaas.bm.wise-paas.com.cn,api-bill-ensaas.bm.wise-paas.com.cn,api-bill-ews001.bm.wise-paas.com.cn,simon.jp.wise-paas.com,portal-bill-ews001.bm.wise-paas.com.cn,api-mpingress-ews001.bm.wise-paas.com.cn');
+user_pref("network.trr.excluded-domains", 'simon.es.wise-paas.cn,ableclub.advantech.com.tw,employeezone.advantech.com.tw,aclelearning.advantech.com.tw,portal-mp-ensaas.bm.wise-paas.com.cn,portal-catalog-ensaas.bm.wise-paas.com.cn,simon.bm.wise-paas.com.cn,portal-sso-ensaas.bm.wise-paas.com.cn,portal-mpsimon-ews001.bm.wise-paas.com.cn,portal-service-ensaas.bm.wise-paas.com.cn,portal-sso-ews001.bm.wise-paas.com.cn,portal-catalog-ews001.bm.wise-paas.com.cn,api-mp-ensaas.bm.wise-paas.com.cn,mailapp.advantech.com.tw,portal-mp-ews001.bm.wise-paas.com.cn,portal-order-ensaas.bm.wise-paas.com.cn,api-bill-ensaas.bm.wise-paas.com.cn,api-bill-ews001.bm.wise-paas.com.cn,simon.jp.wise-paas.com,portal-bill-ews001.bm.wise-paas.com.cn,api-mpingress-ews001.bm.wise-paas.com.cn,api-mp-ews001.bm.wise-paas.com.cn');
 user_pref("network.trr.early-AAAA", true);
 
 // user_pref("dom.webgpu.enabled", true);
